0cb9645c5b0f2741b40f139dac934bcd9445d3f3
Jotski <jotski86@gmail.com>
2015-08-03 13:35:37 +0300

    Jotski git test

--- app/src/main/java/swapper/swapapp/MainActivity.java
+++ app/src/main/java/swapper/swapapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 //testing git
+    //Jotski git push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
